751232640ce83c3e536e3826dc06cfd067cc4c35
jun <chchdelm3@icloud.com>
2021-12-06 14:39:28 +0900

    arduino

---
+++ pfc.ino
@@ -0,0 +1,96 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 7, 6, 3, 2);
+int pin = 13;
+float rads = 57.29577951; // 1 radian = approx 57 deg.
+float degree = 360;
+float frequency = 50;
+float nano = 1 * pow (10,-6); // Multiplication factor to convert micro seconds into seconds
+
+// Define floats to contain calculations
+
+float pf;
+float angle;
+float pf_max = 0;
+float angle_max = 0;
+int ctr;
+int capState1 = 0;
+int capState2 = 0;
+int Relaypin1 = 5;
+int Relaypin2 = 4;
+
+void setup()
+{
+ pinMode(pin, INPUT);
+ Serial.begin(9600);
+ lcd.begin(16, 2);
+ pinMode(Relaypin1, OUTPUT);
+ pinMode(Relaypin2, OUTPUT);
+}
+
+void loop()
+{
+ 
+for (ctr = 0; ctr <= 4; ctr++) // Perform 4 measurements then reset
+  {
+  // 1st line calculates the phase angle in degrees from differentiated time pulse
+  // Function COS uses radians not Degree's hence conversion made by dividing angle / 57.2958
+   angle = ((((pulseIn(pin, HIGH)) * nano)* degree)* frequency);
+  // pf = cos(angle / rads);
+  
+   if (angle > angle_max) // Test if the angle is maximum angle
+     {
+      angle_max = angle; // If maximum record in variable "angle_max"
+      pf_max = cos(angle_max / rads); // Calc PF from "angle_max"
+     }
+   }
+   if (angle_max > 360) // If the calculation is higher than 360 do following...
+   {
+    angle_max = 0; // assign the 0 to "angle_max"
+    pf_max = 1; // Assign the Unity PF to "pf_max"
+   }
+   if (angle_max == 0) // If the calculation is higher than 360 do following...
+   {
+    angle_max = 0; // assign the 0 to "angle_max"
+    pf_max = 1; // Assign the Unity PF to "pf_max"
+   }
+   Serial.print(angle_max, 2); // Print the result
+   Serial.print(",");
+   Serial.println(pf_max, 2);
+   lcd.clear();
+   lcd.setCursor(0,0);
+   lcd.print("PF=");
+   lcd.setCursor(4,0);
+   lcd.print(pf_max);
+   lcd.print(" ");
+   lcd.setCursor(0,1);
+   lcd.print("Ph-Shift=");
+   lcd.setCursor(10,1);
+   lcd.print(angle_max);
+   lcd.print(" ");
+
+   if(pf_max <= 0.98)
+   {
+    if(capState1 == 1 and capState2 == 0)
+    {
+       digitalWrite(Relaypin2,HIGH);
+       delay(200);
+       capState2 = 1;
+    }
+    digitalWrite(Relaypin1,HIGH);
+    delay(200);
+    capState1 = 1;
+   }
+   /*
+   else
+   {
+    digitalWrite(Relaypin1,LOW);
+    digitalWrite(Relaypin2,LOW);
+    delay(100);
+    capState1 = 0;
+    capState2 = 0;
+   }
+   */
+   delay(500);
+   angle = 0; // Reset variables for next test
+   angle_max = 0;
+}
