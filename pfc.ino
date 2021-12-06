1a4935f4ebf022c0ae60aaf3d45b028a6e4393ab
jun <chchdelm3@icloud.com>
2021-12-06 18:59:02 +0900

    add acs and zmtp codes

--- pfc.ino
+++ pfc.ino
@@ -1,12 +1,15 @@
 #include <LiquidCrystal.h>
-LiquidCrystal lcd(12, 11, 7, 6, 3, 2);
-int pin = 13;
-float rads = 57.29577951; // 1 radian = approx 57 deg.
+#define Current_Pin A0
+#define Voltage_Pin A0
+#define ZeroCrossing_pin 13
+#define Relay1_Pin 12
+#define Relay2_Pin 11
+
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+float rads = 57.29577951; // 1 radian
 float degree = 360;
 float frequency = 50;
-float nano = 1 * pow (10,-6); // Multiplication factor to convert micro seconds into seconds
-
-// Define floats to contain calculations
+float micro = 1 * pow (10,-6); // Multiplication factor to convert micro seconds into seconds
 
 float pf;
 float angle;
@@ -15,82 +18,69 @@ float angle_max = 0;
 int ctr;
 int capState1 = 0;
 int capState2 = 0;
-int Relaypin1 = 5;
-int Relaypin2 = 4;
 
 void setup()
 {
- pinMode(pin, INPUT);
- Serial.begin(9600);
- lcd.begin(16, 2);
- pinMode(Relaypin1, OUTPUT);
- pinMode(Relaypin2, OUTPUT);
+  pinMode(ZeroCrossing_pin, INPUT);
+  pinMode(Relay1_Pin, OUTPUT);
+  pinMode(Relay2_Pin, OUTPUT);
+  Serial.begin(9600);
+  lcd.begin(16, 2);
 }
 
 void loop()
 {
- 
-for (ctr = 0; ctr <= 4; ctr++) // Perform 4 measurements then reset
+  for (ctr = 0; ctr <= 4; ctr++) // 4번 측정 수행하고 리셋
   {
-  // 1st line calculates the phase angle in degrees from differentiated time pulse
-  // Function COS uses radians not Degree's hence conversion made by dividing angle / 57.2958
-   angle = ((((pulseIn(pin, HIGH)) * nano)* degree)* frequency);
-  // pf = cos(angle / rads);
-  
-   if (angle > angle_max) // Test if the angle is maximum angle
-     {
-      angle_max = angle; // If maximum record in variable "angle_max"
-      pf_max = cos(angle_max / rads); // Calc PF from "angle_max"
-     }
-   }
-   if (angle_max > 360) // If the calculation is higher than 360 do following...
-   {
+    // 1st line calculates the phase angle in degrees from differentiated time pulse
+    // Cos 함수가 라디안을 사용하므로 angle/57.2958로 변환시켜줌
+    angle = ((((pulseIn(pin, HIGH)) * micro)* degree)* frequency);
+    // pf = cos(angle / rads);
+    if (angle > angle_max) // Test if the angle is maximum angle
+    {
+      angle_max = angle; // 각도의 최댓값을 대입
+      pf_max = cos(angle_max / rads); // "angle_max" 로 부터 역률을 계산
+    }
+  }
+  if (angle_max > 360) // If the calculation is higher than 360 do following...
+  {
     angle_max = 0; // assign the 0 to "angle_max"
     pf_max = 1; // Assign the Unity PF to "pf_max"
-   }
-   if (angle_max == 0) // If the calculation is higher than 360 do following...
-   {
+  }
+  if (angle_max == 0) // If the calculation is higher than 360 do following...
+  {
     angle_max = 0; // assign the 0 to "angle_max"
     pf_max = 1; // Assign the Unity PF to "pf_max"
-   }
-   Serial.print(angle_max, 2); // Print the result
-   Serial.print(",");
-   Serial.println(pf_max, 2);
-   lcd.clear();
-   lcd.setCursor(0,0);
-   lcd.print("PF=");
-   lcd.setCursor(4,0);
-   lcd.print(pf_max);
-   lcd.print(" ");
-   lcd.setCursor(0,1);
-   lcd.print("Ph-Shift=");
-   lcd.setCursor(10,1);
-   lcd.print(angle_max);
-   lcd.print(" ");
+  }
+  Serial.print(angle_max, 2); // Print the result
+  Serial.print(",");
+  Serial.println(pf_max, 2);
+  // lcd.clear();
+  // lcd.setCursor(0,0);
+  // lcd.print("PF=");
+  // lcd.setCursor(4,0);
+  // lcd.print(pf_max);
+  // lcd.print(" ");
+  // lcd.setCursor(0,1);
+  // lcd.print("Ph-Shift=");
+  // lcd.setCursor(10,1);
+  // lcd.print(angle_max);
+  // lcd.print(" ");
 
-   if(pf_max <= 0.98)
-   {
+  // 역률 0.98이하면 릴레이 ON 
+  if(pf_max <= 0.98)
+  {
     if(capState1 == 1 and capState2 == 0)
     {
-       digitalWrite(Relaypin2,HIGH);
-       delay(200);
-       capState2 = 1;
+      digitalWrite(Relaypin2,HIGH);
+      delay(100);
+      capState2 = 1;
     }
     digitalWrite(Relaypin1,HIGH);
-    delay(200);
-    capState1 = 1;
-   }
-   /*
-   else
-   {
-    digitalWrite(Relaypin1,LOW);
-    digitalWrite(Relaypin2,LOW);
     delay(100);
-    capState1 = 0;
-    capState2 = 0;
-   }
-   */
-   delay(500);
-   angle = 0; // Reset variables for next test
-   angle_max = 0;
+    capState1 = 1;
+  }
+  delay(200);
+  angle = 0; // 다음 측정을 위해 각도 0으로 리셋
+  angle_max = 0;
 }
